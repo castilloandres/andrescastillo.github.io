fd726f8bb33283efa2a76ee58c1cc9c0342a3081
ormaechea <castilloandres17@gmail.com>
2018-12-28 13:50:40 +0100

    Add service worker to the homepage

---
+++ sw.js
@@ -0,0 +1,41 @@
+
+const cacheName = `castilloandres.com`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/about.html',
+        '/projects.html',
+        '/assets/css/main.css',
+        '/assets/js/main.js',
+        '/images/blog/mobile-friendly/bad-font-size.png',
+        '/assets/js/jquery.min.js',
+        '/assets/js/jquery.scrollex.min.js',
+        '/assets/js/jquery.scrolly.min.js',
+        '/assets/js/skel.min.js',
+        '/assets/js/util.js',
+        '/assets/css/main.css',
+        '/assets/css/font-awesome.min.css',
+        '/images/banner.jpg',
+        '/images/andres-castillo-think-outside-box.png'
+       ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
